5810ed9635d5353c5d0204465ab97da7d1b94895
P-A-CHATHURA-MADHAVA <ctechict101@gmail.com>
2023-04-04 19:31:32 +0530

    Add Servlets

---
+++ src/main/java/lk/ijse/dep9/api/StudentServlet.java
@@ -0,0 +1,25 @@
+package lk.ijse.dep9.api;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "StudentServlet", value = "/students")
+public class StudentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep9/api/util/HttpServlet2.java
@@ -0,0 +1,23 @@
+package lk.ijse.dep9.api.util;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HttpServlet2 extends HttpServlet {
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(req.getMethod().toUpperCase().equals("PATCH")){
+            doPatch(req, resp);
+        }else {
+            super.service(req, resp);
+        }
+    }
+}
